9fc07737dd987a55c79108dfac7a49760714db21
jeremyblanchard <jeremy.blanchard@klaviyo.com>
2025-11-18 16:20:03 -0500

    Create machine-local.lua

---
+++ machine-local.lua
@@ -0,0 +1,6 @@
+-- Machine-specific configuration (gitignored)
+-- Set this to "work" or "personal" to load the corresponding apps file
+
+return {
+  profile = "work"  -- Change to "personal" on your other computer
+}

a435948f26039eb9365fed4e1f08bfaa606740ad
jeremyblanchard <jjmblanchard@gmail.com>
2025-12-14 09:23:34 -0500

    untrack machine-local.lua

--- machine-local.lua
+++
@@ -1,6 +0,0 @@
--- Machine-specific configuration (gitignored)
--- Set this to "work" or "personal" to load the corresponding apps file
-
-return {
-  profile = "work"  -- Change to "personal" on your other computer
-}

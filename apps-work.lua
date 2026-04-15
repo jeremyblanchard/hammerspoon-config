f891479ac07580deb7700f5e48182312a6f4aecb
jeremyblanchard <jeremy.blanchard@klaviyo.com>
2026-04-15 13:06:43 -0400

    added cmux instead of ghostty

--- apps-work.lua
+++ apps-work.lua
@@ -2,15 +2,16 @@
 -- Format: ["key"] = "Application Name" or "com.bundle.id"
 
 return {
-  ["A"] = "Google Chrome",
-  ["S"] = "Slack",
-  ["D"] = "Ghostty",
-  ["F"] = "Zed",
-  ["G"] = "GitHub Desktop",
-  ["Z"] = "zoom.us",
-  ["X"] = "1Password",
-  ["C"] = "Finder",
-  ["V"] = "Notion Calendar",
-  ["B"] = "System Settings",
-  ["O"] = "Obsidian"
+    ["A"] = "Google Chrome",
+    ["S"] = "Slack",
+    ["D"] = "cmux",
+    ["F"] = "Zed",
+    ["G"] = "GitHub Desktop",
+    ["Z"] = "zoom.us",
+    ["X"] = "1Password",
+    ["C"] = "Finder",
+    ["V"] = "Notion Calendar",
+    ["B"] = "System Settings",
+    ["O"] = "Obsidian",
+    ["L"] = "Linear"
 }
